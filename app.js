5192fd675c76165091233fc4603fb1d4393f2208
Sneh16Shah <snehshah1675@gmail.com>
2022-07-08 13:30:29 +0530

    hm

--- app.js
+++ app.js
@@ -24,11 +24,13 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/railDB';
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/railDB";
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
