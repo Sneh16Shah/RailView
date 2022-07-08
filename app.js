285398026d67002dca4371e83f79b0bc475dcfa7
Sneh16Shah <snehshah1675@gmail.com>
2022-07-08 12:15:07 +0530

    hue

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/railDB';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
 });
 
 const db = mongoose.connection;
